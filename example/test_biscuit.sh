d95545ad48919b9d1fbaf90374039c8ff344b53d
Jiantao Shi <shijiantao@gmail.com>
2024-07-24 15:19:15 +0800

    BISCUIT

---
+++ example/test_biscuit.sh
@@ -0,0 +1,7 @@
+python run_BISCUIT.py -R1 FASTQ/Z000000PX_L001_R1.fastq.gz -R2 FASTQ/Z000000PX_L001_R2.fastq.gz \
+	-N 12 \
+	-I /sibcb3/bioinformatics3/shijiantao/iGenome/BISCUIT/GRCh38/GRCh38 \
+	-R /sibcb3/bioinformatics3/shijiantao/iGenome/BISCUIT/GRCh38/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
+	-tag Z000000PX \
+	-O Z000000PX \
+	--runQC
---
+++ run_BISCUIT.py
@@ -0,0 +1,62 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-R1', "--Fastq_R1", required=True,  help="Read 1 fastq file.")
+parser.add_argument('-R2', "--Fastq_R2", required=False, help="Read 2 fastq file.")
+parser.add_argument('-N', "--threadN", type=int, default=8, required=False, help="Number of threads.")
+parser.add_argument('-I', "--index", required=True,  help="BISCUIT index.")
+parser.add_argument('-R', "--reference", required=True,  help="Reference in FASTA format.")
+parser.add_argument("-runQC", "--runQC", help="whether run QC script", action="store_true")
+parser.add_argument('-O',  "--outFolder", required=True, help="Output folder.")
+parser.add_argument('-tag',  "--tag", required=True, help="Output prefix.")
+args = parser.parse_args()
+
+import sys,os,glob,re, uuid
+
+if not os.path.exists(args.outFolder):
+    os.makedirs(args.outFolder)
+
+biscuit = '/sibcb1/bioinformatics/shijiantao/miniforge3/bin/biscuit'
+MethylDackel = '/sibcb3/bioinformatics3/shijiantao/miniforge3/bin/MethylDackel'
+
+# alignment and deduplication
+prefix_out = f'{args.outFolder}/{args.tag}'
+
+cmd = f'{biscuit} align -@ {args.threadN} {args.index} {args.Fastq_R1}'
+if args.Fastq_R2 is not None:
+	cmd += f' {args.Fastq_R2} | dupsifter {args.reference}'
+cmd += f' | samtools view -b -o {prefix_out}_dup.bam'
+print(cmd)
+os.system(cmd)
+
+# sort
+bam_out = f'{prefix_out}.bam'
+cmd = f'samtools sort -@ {args.threadN} -o {bam_out} {prefix_out}_dup.bam'
+print(cmd)
+os.system(cmd)
+
+# QC
+if args.runQC:
+	cmd = '/sibcb1/bioinformatics/shijiantao/miniforge3/bin/biscuit qc'
+	cmd += f' {args.reference} {bam_out} {prefix_out}'
+	print(cmd)
+	os.system(cmd)
+
+# Extract methylation
+mCall = args.outFolder + '/mCall/'
+if not os.path.exists(mCall):
+    os.makedirs(mCall)
+opref = mCall + args.tag
+cmd = f'{MethylDackel} extract {args.reference} {bam_out} --mergeContext --opref {opref}'
+print(cmd)
+os.system(cmd)
+cmd = f'{MethylDackel} extract {args.reference} {bam_out} --opref {opref} --CHG'
+print(cmd)
+os.system(cmd)
+cmd = f'{MethylDackel} extract {args.reference} {bam_out} --opref {opref} --CHH'
+print(cmd)
+os.system(cmd)
+
+# clean
+cmd = f'rm {prefix_out}_dup.bam'
+os.system(cmd)
