89186667a67a332bcac08fc9867e69446c0f5710
Jiantao Shi <shijiantao@gmail.com>
2024-08-07 16:45:41 +0800

    Update test_wgbsTools.sh

--- example/test_wgbsTools.sh
+++ example/test_wgbsTools.sh
@@ -3,3 +3,13 @@ python run_wgbsTools.py -I wgbs_tools/tutorial/bams/Left_Ventricle_STL001.IGF2.b
 	--CpG hg19_CpG.gz \
 	--mHap -O . \
 	--tag Left_Ventricle_STL001
+
+python run_snpCov.py \
+	-I /sibcb1/bioinformatics/shijiantao/WGBS_Normal/alignment/Z0000043W/Z0000043W.bam \
+	-S out/dbsnp_146.hg38.common_nonCpG.vcf \
+	-C hg38_CpG.gz
+	
+/sibcb2/bioinformatics/software/Miniconda3/bin/python run_snpCov.py \
+	-I /sibcb1/bioinformatics/shijiantao/WGBS_Normal/alignment/Z0000043W/Z0000043W.bam \
+	-S out/dbsnp_146.hg38.common_nonCpG.vcf \
+	-C hg38_CpG.gz > res/Z0000043W_cov.txt
